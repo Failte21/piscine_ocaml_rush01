2d6f69c0fdc389d2458f92d5f9bb282d7d7bf2a2
Leo SIMON <lsimon@e1r11p17.42.fr>
2019-10-26 17:21:55 +0200

    gui: basic buttons understanding

--- src/Gui/Gui.ml
+++ src/Gui/Gui.ml
@@ -6,15 +6,26 @@ let gui gameState () =
   let vbox = new vbox in
   let button = new button
     ~brackets:("[ ", " ]")
-    "exit退出"
+    "exit"
   in
   let label = new label "_" in
   button#on_click (wakeup wakener);
   vbox#add button;
   vbox#add label;
+
+  let hbox = new hbox in
+  let rec display_buttons i =
+    if i <= 0 then () else
+    let button = new button ("button" ^ string_of_int i) in
+    button#on_click (fun () -> print_endline "hello");
+    hbox#add button;
+    display_buttons (i - 1) in
+  display_buttons 4;
+  vbox#add hbox;
+
   let frame = new frame in
   frame#set vbox;
-  frame#set_label ~alignment:LTerm_geom.H_align_center "Button test按钮测试";
+  frame#set_label ~alignment:LTerm_geom.H_align_center "Tamagotchu";
   Lazy.force LTerm.stdout >>= fun term ->
   LTerm.enable_mouse term >>= fun () ->
   Lwt.finalize 
