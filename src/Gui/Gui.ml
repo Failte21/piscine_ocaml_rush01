f17805badc5afbe536c6ed5e21f6c7129d522cda
Leo SIMON <lsimon@e1r11p17.42.fr>
2019-10-26 17:36:05 +0200

    gui: some labels

--- src/Gui/Gui.ml
+++ src/Gui/Gui.ml
@@ -13,15 +13,24 @@ let gui gameState () =
   vbox#add button;
   vbox#add label;
 
-  let hbox = new hbox in
+  let stat_box = new hbox in
+  let rec create_stats i =
+    if i <= 0 then () else
+    let label = new label "Health\n|||||||||||||||||||||||||" in
+    stat_box#add label;
+    create_stats (i - 1) in
+  create_stats 4;
+
+  let buttons_box = new hbox in
   let rec display_buttons i =
     if i <= 0 then () else
     let button = new button ("button" ^ string_of_int i) in
     button#on_click (fun () -> print_endline "hello");
-    hbox#add button;
+    buttons_box#add button;
     display_buttons (i - 1) in
   display_buttons 4;
-  vbox#add hbox;
+  vbox#add stat_box;
+  vbox#add buttons_box;
 
   let frame = new frame in
   frame#set vbox;
