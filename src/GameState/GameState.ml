d237186dfcbb27135a09f5fa328a96a8c21f400d
erobert <etiennerobert33@gmail.com>
2019-10-27 17:07:21 +0100

    remove bool is_over from creature

--- src/GameState/GameState.ml
+++ src/GameState/GameState.ml
@@ -1,24 +1,15 @@
 type t = {
-  is_over: bool;
   creature: Creature.t
 }
 
 let create () = {
-  is_over = false;
   creature = Creature.create ()
 }
 
-let serialize { is_over; creature } =
-  string_of_bool is_over ^ " " ^
+let serialize { creature } =
   Creature.serialize creature
 
 let deserialize s =
-  match String.split_on_char ' ' s with
-  | s_is_over::s_creature::[] ->
-    (match Stdlib.bool_of_string_opt s_is_over,
-           Creature.deserialize s_creature with
-    | None, _ | _, None -> None
-    | Some is_over, Some creature ->
-      Some { is_over = is_over;
-             creature = creature })
-  | _ -> None
+  match Creature.deserialize s with
+  | None -> None
+  | Some creature -> Some { creature = creature }
