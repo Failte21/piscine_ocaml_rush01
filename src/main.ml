ddfbb0f769157a0228eaf8aee5b531917c9c968e
erobert <etiennerobert33@gmail.com>
2019-10-27 22:42:20 +0100

    put highscore management at the begining

--- src/main.ml
+++ src/main.ml
@@ -37,34 +37,20 @@ let get_game_state () : GameState.t =
        (Printexc.to_string e));
     GameState.create ()
 
-let update_high_scores (gameState:GameState.t) =
+let update_high_scores (gameState:GameState.t) name =
   let high_scores_file = "high.scores" in
   let new_high_score hs =
-    print_endline ("Congratulations! You made a new high score: " ^
-      Int.to_string (Float.to_int gameState.time) ^ " !");
-    let rec get_name () =
-      print_string "Enter your name: ";
-      match read_line () with
-      | "" -> get_name ()
-      | name -> name
-      in
     let hs = HighScores.add_score hs
-        { name = get_name ();
+        { name = name;
           time = gameState.time } in
-    HighScores.print hs;
-    match HighScores.save hs high_scores_file with
-    | Ok () -> ()
-    | Error s ->
-      prerr_endline ("An error happened while writing High Scores: " ^ s)
+    ignore (HighScores.save hs high_scores_file)
   in
   let hs_op =
     (if Sys.file_exists high_scores_file
      then
        (match HighScores.load high_scores_file with
         | Ok hs -> Some hs
-        | Error s ->
-          prerr_endline ("An error happened while reading High Scores: " ^ s);
-          None)
+        | Error s -> None)
      else Some (HighScores.create ()))
   in
   Option.iter
@@ -73,11 +59,23 @@ let update_high_scores (gameState:GameState.t) =
        then new_high_score hs)
     hs_op
 
+let rec get_name () =
+  print_string "Enter your name: ";
+  match read_line () with
+  | "" -> get_name ()
+  | name -> name
+
 let main () =
   try
+    print_endline "---- High Scores ----";
+    (match HighScores.load "high.scores" with
+    | Ok hs -> HighScores.print hs
+    | Error e ->
+      prerr_endline ("An error happened while reading High Scores: " ^ e));
+    let name = get_name () in
     let gameState = ref (get_game_state ()) in
     Lwt_main.run (Gui.gui gameState ());
-    update_high_scores !gameState
+    update_high_scores !gameState name
   with e -> prerr_endline ("An error happened: " ^ Printexc.to_string e)
 
 let () = main ()
