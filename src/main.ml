be42aa2d6be3b9e475d69b9415b39023cafbae6b
erobert <etiennerobert33@gmail.com>
2019-10-27 16:37:08 +0100

    Add prompt when backup file is found

--- src/main.ml
+++ src/main.ml
@@ -1,22 +1,38 @@
 let backup_file = "save.itama"
 
-let get_game_state () : GameState.t =
-  if Sys.file_exists backup_file then
-    (match Backup.load backup_file with
-     | Ok gs -> gs
-     | Error s ->
-       prerr_endline ("An error happened while loading file: " ^ s);
-       GameState.create ())
-  else GameState.create ()
+type user_action = LoadFile | NewGame | DeleteFile
 
-let main () =
-  let gameState = get_game_state () in
-  Lwt_main.run (Gui.gui gameState ());
-  try
-    Sys.remove backup_file
+let rec get_user_action () =
+  print_string
+    "A backup file was found. Do you want to load it ? (Y/n/d) ";
+  match read_line () with
+  | "y" | "Y" | "yes" | "" -> LoadFile
+  | "n" | "N" | "no" -> NewGame
+  | "d" | "D" -> DeleteFile
+  | _ -> get_user_action ()
+
+let delete_backup_file () =
+  try Sys.remove backup_file
   with e ->
     prerr_endline
       ("An error happened while removing the backup file: " ^
        Printexc.to_string e)
+
+let get_game_state () : GameState.t =
+  if Sys.file_exists backup_file then
+    match get_user_action () with
+    | LoadFile ->
+      (match Backup.load backup_file with
+       | Ok gs -> gs
+       | Error s ->
+         prerr_endline ("An error happened while loading file: " ^ s);
+         GameState.create ())
+    | NewGame -> GameState.create ()
+    | DeleteFile -> delete_backup_file (); GameState.create ()
+  else GameState.create ()
+
+let main () =
+  let gameState = get_game_state () in
+  Lwt_main.run (Gui.gui gameState ())
 
 let () = main ()
